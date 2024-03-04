6859b2c0eae111d073d655598d60233613e6c0ab
joutinha-7 <160501207+joutinha-7@users.noreply.github.com>
2024-03-04 19:40:58 +0000

    Codigos da aula03 e da pratica1

---
+++ aulas/aula03/tipos.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+  char caracter; // caracteres 'a ', ' A'
+  int  inteiro; // nuemeros interios + -
+  float decimal6; // numero decimal com 6casas
+  double decimal15; // numero decimal com 15casas
+  void sem valor; // sem valor
+  //  nao tem strin, mas aceita "uma frase"
+
+  signed char caracter_com_sinal; // 1byte -128 a 127
+  unsigned char caracter_sem_sinal; // 1byte 0 a 255
+
+  signed int inteiro_com_sinal; // 4bytes -21bi a 21bi
+  unsigned int inteiro_sem_sinal; // 4bytes 0 a 4bi
+
+  short int inteiro_curto; // 2 bytes -32mil a 32mil
+  long int interiro_longo; // 0 bytes -9hexa a 9hexa
+
+  long double duplo_longo; // 16 bytes -1.2E+4932 a 1.2E+4932
+
+  signed short int inteiro_curto_com_sinal; // 2 bytes -32mil a 32mil
+  unsigned short int inteiro_curto_sem_sinal; // 2 bytes 0 a 65mil
+  signed long int inteiro_longo_com_sinal; // 8 bytes -9hexa a 9hexa
+  unsigned long int interio_longo_sem_sinal // 8 bytes 0 a 15hexa
+
+  
+  return 0;
+}
---
+++ aulas/aula03/variaveis_constantes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+  int numero;
+
+  numero = 10;
+
+  numero = 100;
+
+  const float PI = 3.14f;
+
+  PI = 3.1415;
+
+  //PI = 3.1415; NAO PODE!
+
+  int sexo = 1;
+
+  const char SEXO_MASCULINO = 1;
+
+  sexo = SEXO_MASCULINO;
+  
+
+  return 0;
+  
+}
